f8a193e2c6982faec9915a64b2cb7523722730f6
Sebastian Muxel <sebastian@muxel.dev>
2025-09-29 22:47:35 +0200

    cmake: clang: remove red-zone and change freestanding to nostdlib
    Signed-off-by: Sebastian Muxel <sebastian@muxel.dev>

--- cmake/clang-toolchain.cmake
+++ cmake/clang-toolchain.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET aarch64-unknown-elf)
-set(CMAKE_C_FLAGS "-ffreestanding -mno-red-zone")
+set(CMAKE_C_FLAGS "-nostdlib")
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET aarch64-unknown-elf)
 set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
